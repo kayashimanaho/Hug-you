5a3588cbe54cdc0156ce4553baae1ada907b2115
kayashimanaho <116855181+kayashimanaho@users.noreply.github.com>
2023-01-14 15:14:17 +0000

    devise update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,46 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'homes/top'
-  get 'homes/about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root to: "homes#top"
+ # 顧客用
+# URL /users/sign_in ...
+devise_for :users,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+   sessions: "admin/sessions"
+ }
+ 
+ scope module: :public do
+      root to:"homes#top"
+      get "/about" => "homes#about", as: "about"
+      # resources :items, only:[:index, :show]
+      resources :users do
+        collection do
+          get 'my_page'
+          get 'information/edit', action: :edit
+          patch 'information',action: :update
+          get 'unsubscribe' 
+          patch 'withdraw' 
+          end
+      end
+      resources :cart_items, only:[:index, :update, :destroy, :create] do
+        collection do
+          delete 'destroy_all',action: :destroy_all
+        end
+      end
+      resources :orders, only:[:new, :create, :index, :show] do
+        collection do
+          post 'comfirm'
+          get 'complete'
+        end
+      end
+      resources :addresses, only:[:index, :edit, :create, :update, :destroy]
+   end
+   
+   namespace :admin do
+    root to:"homes#top"
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_13_035845) do
+ActiveRecord::Schema.define(version: 2023_01_14_055903) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -40,6 +40,18 @@ ActiveRecord::Schema.define(version: 2023_01_13_035845) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -47,11 +59,11 @@ ActiveRecord::Schema.define(version: 2023_01_13_035845) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name", null: false
-    t.string "nick_name", null: false
-    t.integer "age", null: false
-    t.string "from", null: false
-    t.string "profile_image", null: false
-    t.string "introduction", null: false
+    t.string "nick_name"
+    t.integer "age"
+    t.string "from"
+    t.string "profile_image"
+    t.string "introduction"
     t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
